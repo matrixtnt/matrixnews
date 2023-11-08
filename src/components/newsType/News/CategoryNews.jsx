135721747d2a9354069ab2f806fe8825fc734da2
Sumit-Mayani <sumi.mayani@gmail.com>
2023-11-08 18:08:25 +0530

    category news added

--- src/components/newsType/News/CategoryNews.jsx
+++ src/components/newsType/News/CategoryNews.jsx
@@ -1,87 +1,102 @@
 'use client'
 
 import { FiCalendar } from 'react-icons/fi'
-import { useState } from 'react'
 import Link from 'next/link'
-import { useEffect } from 'react'
 import BreadcrumbNav from '../../breadcrumb/BreadcrumbNav'
-import { getnewsbycategoryApi } from '../../../store/actions/campaign'
 import { useSelector } from 'react-redux'
 import { selectCurrentLanguage } from '../../../store/reducers/languageReducer'
 import Skeleton from 'react-loading-skeleton'
 import { translate } from '../../../utils'
 import { useRouter } from 'next/router.js'
+import { access_key, getLanguage, getUser } from 'src/utils/api'
+import { useQuery } from '@tanstack/react-query'
+import { CategoriesApi } from 'src/hooks/categoriesApi'
+import { useEffect } from 'react'
 
 const CategoryNews = () => {
-  const [Data, setData] = useState([])
   const router = useRouter()
   const query = router.query
   const catId = query.slug
-  const [loading, setLoading] = useState(true)
-  const currentLanguage = useSelector(selectCurrentLanguage)
   const storedLatitude = localStorage.getItem('latitude')
   const storedLongitude = localStorage.getItem('longitude')
-  useEffect(() => {
-    getnewsbycategoryApi(
-      catId,
-      '',
-      '0',
-      '10',
-      storedLatitude && storedLatitude ? storedLatitude : null,
-      storedLongitude && storedLongitude ? storedLongitude : null,
-      response => {
-        setData(response.data)
-        setLoading(false)
-      },
-      error => {
-        if (error === 'No Data Found') {
-          setData('')
-          setLoading(false)
-        }
-      }
+  let user = getUser()
+  let { id: language_id } = getLanguage()
+  useSelector(selectCurrentLanguage)
+
+  // api call
+  const getNewsByCategoryApi = async () => {
+    try {
+      const { data } = await CategoriesApi.getNewsByCategory({
+        access_key: access_key,
+        category_id: catId,
+        subcategory_id: '',
+        offset: '0',
+        limit: '10',
+        user_id: user,
+        language_id: language_id,
+        latitude: storedLatitude ? storedLatitude : null,
+        longitude: storedLongitude ? storedLongitude : null
+      })
+      return data.data
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  // react query
+  const { refetch,isLoading, isError, data, error } = useQuery({
+    queryKey: ['category-news'],
+    queryFn: getNewsByCategoryApi,
+  })
+
+  // loading
+  if (isLoading) {
+    return (
+      <span>
+        <Skeleton height={200} count={3} />
+      </span>
     )
-    // eslint-disable-next-line
-  }, [catId, currentLanguage])
+  }
+
+  // error
+  if (isError) {
+    return <span className='text-center my-5'>{translate('nodatafound')}</span>
+  }
+
+  useEffect(()=>{
+    refetch()
+  },[catId])
 
   return (
     <section className='categoryview_Section'>
       <BreadcrumbNav SecondElement={'category' ? 'category' : ''} ThirdElement='0' />
       <div id='cv-main' className='bg-white py-5'>
         <div id='cv-content' className='my-5 container'>
-          {loading ? (
-            <div>
-              <Skeleton height={200} count={3} />
-            </div>
-          ) : (
-            <div className='row'>
-              {Data.length > 0 ? (
-                Data.map(element => (
-                  <div className='col-lg-3 col-md-4 col-12 ' key={element.id}>
-                    <Link id='Link-all' href={`/news/${element.id}`}>
-                      <div id='cv-card' className='card'>
-                        <img id='cv-card-image' src={element.image} className='card-img' alt='...' />
-                        <div id='cv-card-body' className='card-body'>
-                          <button id='cv-btnCatagory' className='btn btn-sm' type='button'>
-                            {element.category_name}
-                          </button>
-                          <p id='cv-card-title' className='card-title'>
-                            {element.title}
-                          </p>
-                          <p id='cv-card-date'>
-                            <FiCalendar size={18} id='cv-logoCalendar' />
-                            {element.date}
-                          </p>
-                          {/* <Link id='btncvRead' className='btn overlay' type="button" href="/news-view" ><IoArrowForwardCircleSharp size={50}/></Link> */}
-                        </div>
+          <div className='row'>
+            {data &&
+              data.map(element => (
+                <div className='col-lg-3 col-md-4 col-12 ' key={element.id}>
+                  <Link id='Link-all' href={`/news/${element.id}`}>
+                    <div id='cv-card' className='card'>
+                      <img id='cv-card-image' src={element.image} className='card-img' alt='...' />
+                      <div id='cv-card-body' className='card-body'>
+                        <button id='cv-btnCatagory' className='btn btn-sm' type='button'>
+                          {element.category_name}
+                        </button>
+                        <p id='cv-card-title' className='card-title'>
+                          {element.title}
+                        </p>
+                        <p id='cv-card-date'>
+                          <FiCalendar size={18} id='cv-logoCalendar' />
+                          {element.date}
+                        </p>
+                        {/* <Link id='btncvRead' className='btn overlay' type="button" href="/news-view" ><IoArrowForwardCircleSharp size={50}/></Link> */}
                       </div>
-                    </Link>
-                  </div>
-                ))
-              ) : (
-                <div className='text-center my-5'>{translate('nodatafound')}</div>
-              )}
-            </div>
-          )}
+                    </div>
+                  </Link>
+                </div>
+              ))}
+          </div>
         </div>
       </div>
     </section>
--- src/hooks/categoriesApi.jsx
+++ src/hooks/categoriesApi.jsx
@@ -1,5 +1,5 @@
 import Api from 'src/api/AxiosInterceptors'
-import { GET_CATEGORIES } from 'src/utils/api'
+import { GET_CATEGORIES, GET_NEWS_BY_CATEGORY } from 'src/utils/api'
 
 export const CategoriesApi = {
   getCategories: requestData => {
@@ -10,6 +10,20 @@ export const CategoriesApi = {
       limit,
       language_id
     })
+  },
+  getNewsByCategory: requestData => {
+    const { access_key, category_id, subcategory_id, offset, limit, user_id, language_id, latitude, longitude } =
+      requestData
+    return Api.post(GET_NEWS_BY_CATEGORY, {
+      access_key: access_key,
+      category_id,
+      subcategory_id,
+      offset,
+      limit,
+      user_id,
+      language_id,
+      latitude,
+      longitude
+    })
   }
 }
-
