fd2751e24086376691a34dc7df2ed86ddd3b068e
Sumit-Mayani <sumi.mayani@gmail.com>
2023-11-17 17:54:44 +0530

    chnage

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,7 @@
 'use client'
-import { useRouter } from 'next/router';
 import FeatureLayout from '../features-Style/FeatureLayout'
-import { useEffect } from 'react';
 
 const Home = () => {
-
   return (
     <>
       <FeatureLayout />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,5 @@
-'use client'
 import Home from 'src/components/home/Home'
 import generateRssFeed from '../../scripts/generaterRssFeed.mjs'
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
 
 export const getStaticProps = async () => {
   await generateRssFeed()
@@ -12,13 +9,6 @@ export const getStaticProps = async () => {
 }
 
 const index = () => {
-  const router = useRouter()
-  useEffect(() => {
-    // Check if the slug is present in the URL
-    if (router.pathname) {
-      router.replace(window.location.pathname + window.location.search)
-    }
-  }, [])
   return <Home />
 }
 
