dccdc5ff91cb5d926dde2ad6e0e60e208d43b71d
Sumit-Mayani <sumi.mayani@gmail.com>
2023-11-17 11:52:56 +0530

    change

--- src/components/seo/Meta.jsx
+++ src/components/seo/Meta.jsx
@@ -1,43 +1,46 @@
 import Head from 'next/head'
 
-const Meta = ({ title, description, keywords, ogImage, pathName,schema }) => {
-    return (
-        <Head>
-            <title>{title}</title>
-            {/*<meta name="description" content={description} />*/}
+const Meta = ({ title, description, keywords, ogImage, pathName, schema }) => {
+  return (
+    <Head>
+      {/* title */}
+      <title>{title}</title>
 
-            {/*<!-- Google / Search Engine Tags -->*/}
-            <meta name="name" content={title} />
-            <meta name="description" content={description} />
-            <meta name="keywords" content={keywords} />
-            <meta name="image" content={ogImage} />
-            <meta property="og:type" content="website" />
+      {/*<!-- Google / Search Engine Tags -->*/}
+      <meta name='name' content={title} />
+      <meta name='description' content={description} />
+      <meta name='keywords' content={keywords} />
+      <meta name='image' content={ogImage} />
 
-            {/*<!-- Facebook Meta Tags -->*/}
-            <meta property="og:title" content={title} />
-            <meta property="og:description" content={description} />
-            <meta property="og:image" content={ogImage} />
-            <meta property="og:image:type" content="image/jpg" />
-            <meta property="og:image:width" content="1080" />
-            <meta property="og:image:height" content="608" />
-            <meta property="og:url" content={pathName} />
-            <meta property="og:type" content="website" />
+      {/*<!-- Facebook Meta Tags -->*/}
+      <meta property='og:title' content={title} />
+      <meta property='og:description' content={description} />
+      <meta property='og:image' content={ogImage} />
+      <meta property='og:image:type' content='image/jpg' />
+      <meta property='og:image:width' content='1080' />
+      <meta property='og:image:height' content='608' />
+      <meta property='og:url' content={pathName} />
+      <meta property='og:type' content='website' />
 
-            {/*<!-- Twitter Meta Tags -->*/}
-            <meta name="twitter:title" content={title} />
-            <meta name="twitter:description" content={description} />
-            <meta name="twitter:image" content={ogImage} />
-            <meta name="twitter:card" content="summary_large_image" />
-            <link rel='canonical' href={`${process.env.NEXT_PUBLIC_WEB_URL}`} />
-            <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-            <meta name='robots' content='index, follow,max-snippet:-1,max-video-preview:-1,max-image-preview:large' />
-            <script
-          key='structured-data'
-          type='application/ld+json'
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
-        />
-        </Head>
-    )
+      {/*<!-- Twitter Meta Tags -->*/}
+      <meta name='twitter:title' content={title} />
+      <meta name='twitter:description' content={description} />
+      <meta name='twitter:image' content={ogImage} />
+      <meta name='twitter:card' content='summary_large_image' />
+
+      {/* robot and cononical */}
+      <link rel='canonical' href={`${process.env.NEXT_PUBLIC_WEB_URL}`} />
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      <meta name='robots' content='index, follow,max-snippet:-1,max-video-preview:-1,max-image-preview:large' />
+
+      {/* schemas */}
+      <script
+        key='structured-data'
+        type='application/ld+json'
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+      />
+    </Head>
+  )
 }
 
 export default Meta
