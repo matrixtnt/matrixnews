44782cb9bea1104c3fa5a840093102dc6b6390c4
Sumit-Mayani <sumi.mayani@gmail.com>
2023-11-20 17:45:32 +0530

    ss

--- src/pages/rss.js
+++ src/pages/rss.js
@@ -1,33 +1,30 @@
-// 'use client'
-// import { useEffect } from 'react'
-// import generateRssFeed from '../../scripts/generaterRssFeed.mjs'
+'use client'
+import { useEffect } from 'react'
+import generateRssFeed from '../../scripts/generaterRssFeed.mjs'
 
-// // export const getStaticProps = async () => {
-// //   await generateRssFeed()
-// //   return {
-// //     props: {}
-// //   }
-// // }
-
-// // export async function getStaticProps({ params }) {
-// //   let data = null;
-// //   try {
-// //     data = await generateRssFeed(params.slug);
-// //   } catch (err) { };
-
-// //   return {
-// //     props: {
-// //       data,
-// //     },
-// //   };
-// // }
-
-// const rss = () => {
-//   useEffect(()=>{
-//     generateRssFeed()
-//   },[])
-
-//   return <div>rss</div>
+// export const getStaticProps = async () => {
+//   await generateRssFeed()
+//   return {
+//     props: {}
+//   }
 // }
 
-// export default rss
+// export async function getStaticProps({ params }) {
+//   let data = null;
+//   try {
+//     data = await generateRssFeed(params.slug);
+//   } catch (err) { };
+
+//   return {
+//     props: {
+//       data,
+//     },
+//   };
+// }
+
+const rss = () => {
+
+  return <div>rss</div>
+}
+
+export default rss
