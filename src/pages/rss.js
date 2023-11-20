db20f011278e089d5c537348e892dcd399c91647
Sumit-Mayani <sumi.mayani@gmail.com>
2023-11-20 17:49:20 +0530

    ss

---
+++ src/pages/rss.js
@@ -0,0 +1,29 @@
+'use client'
+import generateRssFeed from '../../scripts/generaterRssFeed.mjs'
+
+export const getStaticProps = async () => {
+  await generateRssFeed()
+  return {
+    props: {}
+  }
+}
+
+// export async function getStaticProps({ params }) {
+//   let data = null;
+//   try {
+//     data = await generateRssFeed(params.slug);
+//   } catch (err) { };
+
+//   return {
+//     props: {
+//       data,
+//     },
+//   };
+// }
+
+const rss = () => {
+
+  return <div>rss</div>
+}
+
+export default rss
