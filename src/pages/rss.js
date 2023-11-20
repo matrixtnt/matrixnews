81d97668af216e366e4a68fe966ccf67bd9f8d10
Sumit-Mayani <sumi.mayani@gmail.com>
2023-11-20 17:41:56 +0530

    ss

--- src/pages/rss.js
+++ src/pages/rss.js
@@ -1,12 +1,13 @@
 'use client'
+import { useEffect } from 'react'
 import generateRssFeed from '../../scripts/generaterRssFeed.mjs'
 
-export const getStaticProps = async () => {
-  await generateRssFeed()
-  return {
-    props: {}
-  }
-}
+// export const getStaticProps = async () => {
+//   await generateRssFeed()
+//   return {
+//     props: {}
+//   }
+// }
 
 // export async function getStaticProps({ params }) {
 //   let data = null;
@@ -22,6 +23,10 @@ export const getStaticProps = async () => {
 // }
 
 const rss = () => {
+  useEffect(()=>{
+    generateRssFeed()
+  },[])
+
   return <div>rss</div>
 }
 
