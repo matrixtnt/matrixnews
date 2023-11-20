c15f8c29dccfd9e550a8120e9b1874abcee5437e
Sumit-Mayani <sumi.mayani@gmail.com>
2023-11-20 17:25:43 +0530

    ss

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -79,14 +79,14 @@ const Layout = ({ children }) => {
   }, [hasToken])
 
   // client side rendering route get
-  useEffect(() => {
-    // Check if the slug is present in the URL
-    if (process.env.NEXT_PUBLIC_SEO === 'false') {
-      if (router.pathname) {
-        router.replace(window.location.pathname + window.location.search)
-      }
-    }
-  }, [process.env.NEXT_PUBLIC_SEO])
+  // useEffect(() => {
+  //   // Check if the slug is present in the URL
+  //   if (process.env.NEXT_PUBLIC_SEO === 'false') {
+  //     if (router.pathname) {
+  //       router.replace(window.location.pathname + window.location.search)
+  //     }
+  //   }
+  // }, [process.env.NEXT_PUBLIC_SEO])
 
   return (
     <>
