f9c84d965143a141f88252f6a5c8649e6cf63d6e
Sumit-Mayani <sumi.mayani@gmail.com>
2024-02-07 18:33:13 +0530

    categories issue solved

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -12,85 +12,108 @@ import { CategoriesApi } from 'src/hooks/categoriesApi'
 import { access_key } from 'src/utils/api'
 import { useQuery } from '@tanstack/react-query'
 import Layout from '../layout/Layout'
+import Card from '../skeletons/Card'
 
 const Categories = () => {
+  const [currentPage, setCurrentPage] = useState(0)
+  const dataPerPage = 9
   const currentLanguage = useSelector(selectCurrentLanguage)
   const categoiresOnOff = useSelector(settingsData)
-  const [totalLength, setTotalLength] = useState(0)
-  const [offsetdata, setOffsetdata] = useState(0)
-  const limit = 12
 
-  const handlePageChange = selectedPage => {
-    const newOffset = selectedPage.selected * limit
-    setOffsetdata(newOffset)
+  // handle page change
+  const handlePageChange = ({ selected }) => {
+    setCurrentPage(selected)
   }
 
   // api call
-  const categoriesApi = async () => {
+  const categoriesApi = async page => {
     try {
       const { data } = await CategoriesApi.getCategories({
         access_key,
-        offset: offsetdata.toString(),
-        limit: limit.toString(),
+        offset: page * dataPerPage,
+        limit: dataPerPage,
         language_id: currentLanguage.id
       })
-      setTotalLength(data.total)
-      return data.data
+      return data
     } catch (error) {
       console.log(error)
     }
   }
 
   // react query
-  const { data } = useQuery({
-    queryKey: ['categories', offsetdata.toString(), limit.toString(), currentLanguage.id],
-    queryFn: categoriesApi,
+  const { isLoading, data: Data } = useQuery({
+    queryKey: ['categories', currentPage, currentLanguage.id],
+    queryFn: () => categoriesApi(currentPage)
   })
 
+  // slice the array to get the current posts
+  const currentData = Data && Data.data && Data.data.slice(0, dataPerPage)
+
+  const lengthdata = (Data && Data.total) || 0
 
   return (
     <Layout>
       <BreadcrumbNav SecondElement='Categories' ThirdElement='0' />
       {categoiresOnOff && categoiresOnOff.category_mode === '1' ? (
         <div className='container my-5'>
-          <div className='row'>
-            {data &&
-              data.map(element => (
-                <div className='col-md-4 col-12 mb-4'>
-                  <Link id='cat-section-card' key={element.id} className='card' href={`/categories-news/${element.slug}`}>
-                    <img id='cat-section-card-image' src={element.image} className='categories card news image' alt={element.category_name} />
-                    <div id='cat-section-card-body' className='card-img-overlay'>
-                      <h5 id='cat-card-text' className='card-text mb-0'>
-                        {element.category_name}
-                      </h5>
-                      <button id='btn-cat-more' className='btn' type='button'>
-                        <IoArrowForwardCircleSharp size={40} />
-                      </button>
-                    </div>
-                  </Link>
+          {isLoading ? (
+            <div className='row'>
+              {[...Array(3)].map((_, index) => (
+                <div className='col-md-4 col-12' key={index}>
+                  <Card isLoading={true} />
                 </div>
               ))}
-          </div>
-          <ReactPaginate
-            previousLabel={translate('previous')}
-            nextLabel={translate('next')}
-            breakLabel='...'
-            breakClassName='break-me'
-            pageCount={Math.ceil(totalLength / limit)}
-            marginPagesDisplayed={2}
-            pageRangeDisplayed={5}
-            onPageChange={handlePageChange}
-            containerClassName={'pagination'}
-            previousLinkClassName={'pagination__link'}
-            nextLinkClassName={'pagination__link'}
-            disabledClassName={'pagination__link--disabled'}
-            activeClassName={'pagination__link--active'}
-          />
+            </div>
+          ) : (
+            <div className='row'>
+              {currentData && currentData.length > 0 ? (
+                currentData.map(element => (
+                  <div className='col-md-4 col-12 mb-4'>
+                    <Link
+                      id='cat-section-card'
+                      key={element.id}
+                      className='card'
+                      href={`/categories-news/${element.slug}`}
+                    >
+                      <img
+                        id='cat-section-card-image'
+                        src={element.image}
+                        className='categories card news image'
+                        alt={element.category_name}
+                      />
+                      <div id='cat-section-card-body' className='card-img-overlay'>
+                        <h5 id='cat-card-text' className='card-text mb-0'>
+                          {element.category_name}
+                        </h5>
+                        <button id='btn-cat-more' className='btn' type='button'>
+                          <IoArrowForwardCircleSharp size={40} />
+                        </button>
+                      </div>
+                    </Link>
+                  </div>
+                ))
+              ) : (
+                <div className='text-center my-5'>{translate('nodatafound')}</div>
+              )}
+              {lengthdata != 0 ? (
+                <ReactPaginate
+                  initialPage={currentPage}
+                  previousLabel={translate('previous')}
+                  nextLabel={translate('next')}
+                  pageCount={Math.ceil(lengthdata / dataPerPage)}
+                  onPageChange={handlePageChange}
+                  containerClassName={'pagination'}
+                  previousLinkClassName={'pagination__link'}
+                  nextLinkClassName={'pagination__link'}
+                  disabledClassName={'pagination__link--disabled'}
+                  activeClassName={'pagination__link--active'}
+                />
+              ) : null}
+            </div>
+          )}
         </div>
       ) : (
-        <>
-          <div className='text-center my-5'>{translate('disabledCategory')}</div>
-        </>
+        <div className='text-center my-5'>{translate('nodatafound')}</div>
       )}
     </Layout>
   )
