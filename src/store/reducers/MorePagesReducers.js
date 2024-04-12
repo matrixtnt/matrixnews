d8391f3808641e638b34fc46e9c1f55afc88008f
Sumit-Mayani <sumi.mayani@gmail.com>
2024-04-12 17:51:43 +0530

    more page undefine issue solved

--- src/store/reducers/MorePagesReducers.js
+++ src/store/reducers/MorePagesReducers.js
@@ -3,74 +3,63 @@ import { createSelector, createSlice } from '@reduxjs/toolkit'
 import { store } from '../store'
 import moment from 'moment'
 import { apiCallBegan } from '../actions/apiActions'
+import { getPages } from 'src/utils/api'
 
 // Initial state with some default data
 const initialState = {
-    loading: false,
-    lastFetch: null,
-    data: [],
-    Lang: null,
-
+  data: [],
+  loading: false,
+  Lang: null,
+  lastFetch: null
 }
 
 // Create a Redux slice
 export const morePagesSlice = createSlice({
-    name: 'morePages',
-    initialState,
-    reducers: {
-
-        morePagesRequested: (state, action) => {
-            state.loading = true;
-        },
-        morePagesSuccess: (state, action) => {
-            state.loading = false;
-            state.morePages = action.payload.data;
-            state.lastFetch = Date.now();
-            state.Lang = action.request
-        },
-        morePagesFailed: (state, action) => {
-            state.loading = false;
-        },
+  name: 'morePages',
+  initialState,
+  reducers: {
+    morePagesRequested: (state, action) => {
+      state.loading = true
+    },
+    morePagesSuccess: (state, action) => {
+      state.loading = false
+      state.morePages = action.payload.data
+      state.lastFetch = Date.now()
+      state.Lang = action.request
+    },
+    morePagesFailed: (state, action) => {
+      state.loading = false
     }
+  }
 })
 
-export const {
-    morePagesRequested,
-    morePagesSuccess,
-    morePagesFailed
-} = morePagesSlice.actions
+export const { morePagesRequested, morePagesSuccess, morePagesFailed } = morePagesSlice.actions
 export default morePagesSlice.reducer
 
-
-
 // API CALLS
-export const loadMorePages = ({
-    slug = "",
-    onSuccess = () => { },
-    onError = () => { },
-    onStart = () => { }
-}) => {
-    const { currentLanguage } = store.getState().languages
-    const { lastFetch, Lang } = store.getState().morePages;
-    const diffInMinutes = moment().diff(moment(lastFetch), "minutes");
-    // If API data is fetched within last 10 minutes then don't call the API again
-    if (currentLanguage?.id != Lang?.language_id || diffInMinutes > process.env.NEXT_PUBLIC_LOAD_MIN) {
-        store.dispatch(
-            apiCallBegan({
-                ...getPages(slug),
-                displayToast: false,
-                onStartDispatch: categoriesRequested.type,
-                onSuccessDispatch: categoriesSuccess.type,
-                onErrorDispatch: categoriesFailed.type,
-                onStart,
-                onSuccess,
-                onError,
-            })
-        );
-    }
-};
+export const loadMorePages = ({ slug = '', onSuccess = () => {}, onError = () => {}, onStart = () => {} }) => {
+  const { currentLanguage } = store.getState().languages
+  const { lastFetch, Lang } = store.getState().morePages ?? {};
+  const diffInMinutes = lastFetch ? moment().diff(moment(lastFetch), 'minutes') : 0
+
+  // If API data is fetched within last 10 minutes then don't call the API again
+  if (currentLanguage?.id != Lang?.language_id || diffInMinutes > process.env.NEXT_PUBLIC_LOAD_MIN) {
+    store.dispatch(
+      apiCallBegan({
+        ...getPages(slug),
+        displayToast: false,
+        onStartDispatch: morePagesRequested.type,
+        onSuccessDispatch: morePagesSuccess.type,
+        onErrorDispatch: morePagesFailed.type,
+        onStart,
+        onSuccess,
+        onError
+      })
+    )
+  }
+}
 
 export const getMorePagesData = createSelector(
-    (state) => state.morePages,
-    (morePages) => morePages.data
-);
+  state => state.morePages,
+  morePages => morePages.data
+)
