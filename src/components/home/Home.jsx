746383ec978e5f28ed6529c4f3588262c74b64d9
shahid <wrteamshahid03@gmail.com>
2024-06-03 18:48:02 +0530

    adsense added for seo and without seo

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,21 @@
 'use client'
+import { useSelector } from 'react-redux'
 import FeatureLayout from '../features-Style/FeatureLayout'
 import Layout from '../layout/Layout'
+import { settingsData } from 'src/store/reducers/settingsReducer'
 
 const Home = () => {
+  const adsenseData = useSelector(settingsData)
+  const adsenseUrl = adsenseData?.web_setting?.google_adsense;
+
+  console.log(adsenseUrl, '180x180')
+
+  if (process.env.NEXT_PUBLIC_SEO === 'false') {
+    adsenseUrl && adsenseUrl !== null || adsenseUrl && adsenseUrl !== undefined || adsenseUrl && adsenseUrl?.length > 0 ?
+      <script async src={adsenseUrl}
+        crossorigin="anonymous"></script> : null
+  }
+
   return (
     <Layout>
       <FeatureLayout />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,69 @@
+import axios from 'axios'
 import dynamic from 'next/dynamic'
 import Meta from 'src/components/seo/Meta'
+import { GET_SETTINGS } from 'src/utils/api'
 const Home = dynamic(() => import('src/components/home/Home'), { ssr: false })
-const Index = () => {
+
+
+// This is seo api
+const fetchDataFromSeo = async () => {
+  try {
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/${process.env.NEXT_PUBLIC_END_POINT}/${GET_SETTINGS}`
+    )
+    const data = response.data
+    return data
+  } catch (error) {
+    console.error('Error fetching data:', error)
+    return null
+  }
+}
+
+const Index = ({ adsenseUrl }) => {
+
+  // console.log(settingsData,"settingsDataaaaa")
+
   return (
     <>
       <Meta />
       <Home />
+      {
+        adsenseUrl && adsenseUrl !== null || adsenseUrl && adsenseUrl !== undefined || adsenseUrl && adsenseUrl?.length > 0 ?
+          <script async src={adsenseUrl}
+            crossorigin="anonymous"></script> : null
+      }
     </>
   )
 }
+
+
+let serverSidePropsFunction = null
+if (process.env.NEXT_PUBLIC_SEO === 'true') {
+  serverSidePropsFunction = async context => {
+    // Retrieve the slug from the URL query parameters
+    const { req } = context // Extract query and request object from context
+
+    const currentURL = req[Symbol.for('NextInternalRequestMeta')].initURL
+
+    const settingsData = await fetchDataFromSeo()
+
+    // console.log(settingsData?.data, "Data")
+
+    const adsenseUrl = settingsData?.data?.web_setting?.google_adsense;
+
+    // console.log(adsenseUrl, "adsenseUrl")
+
+    // Pass the fetched data as props to the page component
+    return {
+      props: {
+        settingsData,
+        currentURL,
+        adsenseUrl
+      }
+    }
+  }
+}
+
+export const getServerSideProps = serverSidePropsFunction
 
 export default Index
