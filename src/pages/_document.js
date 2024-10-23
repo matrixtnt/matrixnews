e85a028d5725e6ec9a8f92a3498a5b24ac72425d
shahid <wrteamshahid03@gmail.com>
2024-10-23 18:17:07 +0530

    gtm script added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,6 +12,19 @@ const CustomDocument = () => {
         <link rel='apple-touch-icon' sizes='180x180' href='/images/apple-touch-icon.png' />
         <link href="https://fonts.googleapis.com/css2?family=Zilla+Slab:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet"></link>
         <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet"></link>
+
+        {/* Google Tag Manager Script */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process?.env?.NEXT_PUBLIC_MEASUREMENT_ID}`}></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', ${process?.env?.NEXT_PUBLIC_MEASUREMENT_ID});
+            `,
+          }}
+        />
       </Head>
 
       {/* add to any script */}
